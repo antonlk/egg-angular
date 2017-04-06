dd1ed6f8edeb22b2320a7684b22a2ce7407fe7ea
Anton Lokhvitskiy <anton.lokhvitskiy@gmail.com>
2017-04-06 11:38:04 +0200

    Añadida funcionalidad a botones de editar y eliminar, carpeta datos eliminada porque los datos se pasaron a arrays de la app

--- app/categories/category.component.js
+++ app/categories/category.component.js
@@ -1,5 +1,5 @@
-(function() {
-'use strict';
+(function () {
+  'use strict';
 
 
   angular
@@ -9,7 +9,7 @@
       templateUrl: 'app/categories/category.component.html',
       controller: CategoryController,
       bindings: {
-        
+
       }
     })
     ;
@@ -29,94 +29,146 @@
     );
     */
 
-//DATOS
-      $ctrl.categories = [
-  {
-    "id": "0",
-    "name":"Marvel"
-  },
-  {
-    "id": "1",
-    "name":"Kittens"
-  }
+    //DATOS
+    $ctrl.categories = [
+      {
+        "id": "0",
+        "name": "Marvel"
+      },
+      {
+        "id": "1",
+        "name": "Kittens"
+      }
     ];
 
-   $ctrl.links= [
- {
-    "id": "0",
-    "title": "Iron Man",
-    "category": "Marvel",
-    "link":"https://lumiere-a.akamaihd.net/v1/images/3f1baa0812f35ac2910feeaf463a8d9c437a9b19.png?region=0%2C0%2C600%2C600"
-  },
- {
-    "id": "1",
-    "title": "Kitten 1",
-    "category": "Kittens",
-    "link":"http://placekitten.com/280/360"
-  },
- {
-    "id": "2",
-    "title": "Kitten 2",
-    "category": "Kittens",
-    "link":"http://placekitten.com/240/360"
-  },
- {
-    "id": "3",
-    "title": "Pie",
-    "category": "Food",
-    "link":"http://www.guymondailyherald.com/sites/default/files/field/image/apple-pie.jpg"
-  }
-];
-//FIN DATOS
+    $ctrl.images = [
+      {
+        "id": "0",
+        "title": "Iron Man",
+        "category": "Marvel",
+        "link": "https://lumiere-a.akamaihd.net/v1/images/3f1baa0812f35ac2910feeaf463a8d9c437a9b19.png?region=0%2C0%2C600%2C600"
+      },
+      {
+        "id": "1",
+        "title": "Kitten 1",
+        "category": "Kittens",
+        "link": "http://placekitten.com/280/360"
+      },
+      {
+        "id": "2",
+        "title": "Kitten 2",
+        "category": "Kittens",
+        "link": "http://placekitten.com/240/360"
+      },
+      {
+        "id": "3",
+        "title": "Pie",
+        "category": "Food",
+        "link": "http://www.guymondailyherald.com/sites/default/files/field/image/apple-pie.jpg"
+      }
+    ];
+    //FIN DATOS
 
-// CATEGOTY TRACKING
+    // CATEGOTY TRACKING
     $ctrl.currentCategory = null;
 
-      $ctrl.setCurrentCategory = function(category){
-       $ctrl.currentCategory = category;
-        console.log($ctrl.currentCategory);
+    $ctrl.setCurrentCategory = function (category) {
+      $ctrl.currentCategory = category;
+      console.log($ctrl.currentCategory);
 
-        $ctrl.cancelCreating();
-        $ctrl.cancelEditing();
-     }
+      $ctrl.cancelCreating();
+      $ctrl.cancelEditing();
+    }
 
-     $ctrl.isCurrentCategory = function(category){
-       return $ctrl.currentCategory !== null && category.name === $ctrl.currentCategory.name;
-     }
-  
-
-  // END CATEGORY TRACKING
-
-  //CREATE / EDIT TRACKING
-
-  $ctrl.isCreating = false;
-  $ctrl.isEditing = false;
-
-  $ctrl.startCreating = function(){
-    $ctrl.isCreating= true;
-    $ctrl.isEditing=false;
-  }
-
-  $ctrl.cancelCreating=function(){
-    $ctrl.isCreating=false;
-  }
-
-  $ctrl.startEditing=function(){
-    $ctrl.isCreating= false;
-    $ctrl.isEditing=true;
-    // JAVASCRIPT DE SEMANTIC
-
-  $('.ui.modal').modal('show');
-  }
-  $ctrl.cancelEditing=function(){
-    $ctrl.isEditing=false;
-    // JAVASCRIPT DE SEMANTIC
-
-  $('.ui.modal').modal('hide');
-  }
+    $ctrl.isCurrentCategory = function (category) {
+      return $ctrl.currentCategory !== null && category.name === $ctrl.currentCategory.name;
+    }
 
 
-  // END CREATE/EDIT
+    // END CATEGORY TRACKING
+
+    //CREATE / EDIT TRACKING
+
+    $ctrl.isCreating = false;
+    $ctrl.isEditing = false;
+
+    $ctrl.startCreating = function () {
+      $ctrl.isCreating = true;
+      $ctrl.isEditing = false;
+      $ctrl.resetCreateImageForm();
+    }
+
+    $ctrl.cancelCreating = function () {
+      $ctrl.isCreating = false;
+    }
+
+    $ctrl.startEditing = function () {
+      $ctrl.isCreating = false;
+      $ctrl.isEditing = true;
+      // JAVASCRIPT DE SEMANTIC
+
+      $('.ui.modal').modal('show');
+    }
+    $ctrl.cancelEditing = function () {
+      $ctrl.isEditing = false;
+      // JAVASCRIPT DE SEMANTIC
+
+      $('.ui.modal').modal('hide');
+    }
+
+    // END CREATE/EDIT TRACKING
+
+    //CREATE AND ADD NEW IMAGE
+    $ctrl.resetCreateImageForm = function () {
+      $ctrl.newImage = {
+        title: '',
+        link: '',
+        category: $ctrl.currentCategory
+      }
+    }
+
+    $ctrl.createImage = function (image) {
+      image.id = $ctrl.images.length;
+      $ctrl.images.push(image);
+
+      $ctrl.resetCreateImageForm();
+
+      console.log("submit");
+    }
+    // END CREATE / ADD IMAGE
+    // EDIT IMAGE
+
+    $ctrl.editedImage = null;
+
+    $ctrl.setEditedImage = function (image) {
+      $ctrl.editedImage = angular.copy(image);
+      console.log("editing:" + $ctrl.editedImage.title);
+    }
+
+    $ctrl.updateImage = function (image) {
+
+      for (var i = 0; i < $ctrl.images.length; i++) {
+        if (image.id == $ctrl.images[i].id) {
+          $ctrl.images[i] = image;
+          $ctrl.cancelEditing();
+          break;
+        }
+      }
+    }
+    // END EDIT IMAGE
+
+    // DELETE IMAGE
+
+    $ctrl.deleteImage = function(image){
+      for (var i = 0; i < $ctrl.images.length; i++) {
+        if (image.id == $ctrl.images[i].id) {
+          $ctrl.images.splice(i,1);
+          break;
+        }
+      }
+    }
+
+    // END DELETE IMAGE
 
   }
 })();
